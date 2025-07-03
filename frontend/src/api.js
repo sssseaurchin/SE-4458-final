7e2bf333fa4f904ee9cc5da5af5b56dd9e9d181d
sssseaurchin <157834865+sssseaurchin@users.noreply.github.com>
2025-07-03 13:49:02 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3002/api/v1',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
